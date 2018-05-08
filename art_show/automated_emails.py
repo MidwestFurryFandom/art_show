f29c0fad195fc08ae9315d8fa6842e205ad5fc8f
Victoria Earl <kitsuta@gmail.com>
2018-05-08 18:27:16 -0400

    Update automated emails to work with new Fixtures in Uber plugin

--- art_show/automated_emails.py
+++ art_show/automated_emails.py
@@ -1,33 +1,33 @@
 from art_show import *
 
-AutomatedEmail.queries.update({
+AutomatedEmailFixture.queries.update({
     ArtShowApplication: lambda session: session.query(ArtShowApplication)
                                         .options(subqueryload(ArtShowApplication.attendee))
 })
 
 
-class ArtShowAppEmail(AutomatedEmail):
+class ArtShowAppEmailFixture(AutomatedEmailFixture):
     def __init__(self, subject, template, filter, ident, **kwargs):
-        AutomatedEmail.__init__(self, ArtShowApplication, subject, template,
+        AutomatedEmailFixture.__init__(self, ArtShowApplication, subject, template,
                                 lambda app: True and filter(app),
                                 ident, sender=c.ART_SHOW_EMAIL, **kwargs)
 
-ArtShowAppEmail('Your {EVENT_NAME} Art Show application has been approved', 'art_show/approved.html',
+ArtShowAppEmailFixture('Your {EVENT_NAME} Art Show application has been approved', 'art_show/approved.html',
                 lambda a: a.status == c.APPROVED,
                 needs_approval=False,
                 ident='art_show_approved')
 
-ArtShowAppEmail('Your {EVENT_NAME} Art Show application has been waitlisted', 'art_show/waitlisted.txt',
+ArtShowAppEmailFixture('Your {EVENT_NAME} Art Show application has been waitlisted', 'art_show/waitlisted.txt',
                 lambda a: a.status == c.WAITLISTED,
                 needs_approval=False,
                 ident='art_show_waitlisted')
 
-ArtShowAppEmail('Your {EVENT_NAME} Art Show application has been declined', 'art_show/declined.txt',
+ArtShowAppEmailFixture('Your {EVENT_NAME} Art Show application has been declined', 'art_show/declined.txt',
                 lambda a: a.status == c.DECLINED,
                 needs_approval=False,
                 ident='art_show_declined')
 
-ArtShowAppEmail('Reminder to pay for your {EVENT_NAME} Art Show application', 'art_show/payment_reminder.txt',
+ArtShowAppEmailFixture('Reminder to pay for your {EVENT_NAME} Art Show application', 'art_show/payment_reminder.txt',
                 lambda a: a.status == c.APPROVED and a.is_unpaid,
                 when=days_before(14, c.ART_SHOW_PAYMENT_DUE),
                 needs_approval=False,
